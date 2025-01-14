96c07d315f800093cb1608f105b2d87b0b7a63d3
char-tan <76977995+char-tan@users.noreply.github.com>
2025-01-14 00:08:15 +0000

    revert to original egnn dynamics model + shortcut

---
+++ src/models/components/tbg/egnn.py
@@ -0,0 +1,69 @@
+import torch.nn as nn
+
+from src.models.components.tbg.gcl import E_GCL
+
+
+class EGNN(nn.Module):
+    def __init__(
+        self,
+        in_node_nf,
+        in_edge_nf,
+        hidden_nf,
+        device="cpu",
+        act_fn=nn.SiLU(),
+        n_layers=4,
+        recurrent=True,
+        attention=False,
+        norm_diff=True,
+        out_node_nf=None,
+        tanh=False,
+        coords_range=15,
+        agg="sum",
+    ):
+        super().__init__()
+        if out_node_nf is None:
+            out_node_nf = in_node_nf
+        self.hidden_nf = hidden_nf
+        self.device = device
+        self.n_layers = n_layers
+        self.coords_range_layer = float(coords_range) / self.n_layers
+        if agg == "mean":
+            self.coords_range_layer = self.coords_range_layer * 19
+        # self.reg = reg
+        # Encoder
+        # self.add_module("gcl_0", E_GCL(in_node_nf, self.hidden_nf, self.hidden_nf, edges_in_d=in_edge_nf, act_fn=act_fn, recurrent=False, coords_weight=coords_weight))
+        self.embedding = nn.Linear(in_node_nf, self.hidden_nf)
+        self.embedding_out = nn.Linear(self.hidden_nf, out_node_nf)
+        for i in range(0, n_layers):
+            self.add_module(
+                "gcl_%d" % i,
+                E_GCL(
+                    self.hidden_nf,
+                    self.hidden_nf,
+                    self.hidden_nf,
+                    edges_in_d=in_edge_nf,
+                    act_fn=act_fn,
+                    recurrent=recurrent,
+                    attention=attention,
+                    norm_diff=norm_diff,
+                    tanh=tanh,
+                    coords_range=self.coords_range_layer,
+                    agg=agg,
+                ),
+            )
+
+        self.to(self.device)
+
+    def forward(self, h, x, edges, edge_attr=None, node_mask=None, edge_mask=None):
+        # Edit Emiel: Remove velocity as input
+        h = self.embedding(h)
+        for i in range(0, self.n_layers):
+            h, x, _ = self._modules["gcl_%d" % i](
+                h, edges, x, edge_attr=edge_attr, node_mask=node_mask, edge_mask=edge_mask
+            )
+        h = self.embedding_out(h)
+
+        # Important, the bias of the last linear might be non-zero
+        if node_mask is not None:
+            h = h * node_mask
+        return h, x
---
+++ src/models/components/tbg/egnn_dynamics.py
@@ -0,0 +1,102 @@
+import math
+
+import torch
+import torch.nn as nn
+
+from src.models.components.tbg.egnn import EGNN
+from src.models.components.tbg.utils import remove_mean
+
+
+class EGNN_dynamics(nn.Module):
+    def __init__(
+        self,
+        n_particles,
+        n_dimensions,
+        hidden_nf=64,
+        device="cpu",
+        act_fn=torch.nn.SiLU(),
+        n_layers=3,
+        recurrent=True,
+        attention=False,
+        tanh=False,
+        agg="sum",
+        M=128,
+    ):
+        super().__init__()
+        self.egnn = EGNN(
+            in_node_nf=2,
+            in_edge_nf=1,
+            hidden_nf=hidden_nf,
+            device=device,
+            act_fn=act_fn,
+            n_layers=n_layers,
+            recurrent=recurrent,
+            attention=attention,
+            tanh=tanh,
+            agg=agg,
+        )
+
+        self.device = device
+        self._n_particles = n_particles
+        self._n_dimensions = n_dimensions
+        self.edges = self._create_edges()
+        self._edges_dict = {}
+        # Count function calls
+        self.counter = 0
+        self.M = M
+
+    def forward(self, t, x, d_base=None, *args, **kwargs):
+        t = t.view(-1, 1)
+        d_base = d_base.view(-1, 1) if d_base is not None else None
+
+        if t.numel() == 1:
+            t = t.repeat(x.shape[0], 1)
+
+        if d_base is None:
+            d_base = torch.ones_like(t) * math.log2(self.M)  # so it defaults to non-shortcut model
+        elif d_base.numel() == 1:
+            d_base = d_base.repeat(x.shape[0], 1)
+
+        n_batch = x.shape[0]
+        edges = self._cast_edges2batch(self.edges, n_batch, self._n_particles, device=x.device)
+        edges = [edges[0], edges[1]]
+        # Changed by Leon
+        x = x.reshape(n_batch * self._n_particles, self._n_dimensions).clone()
+        h = torch.ones(n_batch, self._n_particles, 2, device=x.device)
+
+        td = torch.cat([t, d_base], dim=-1)
+        h = h * td.unsqueeze(1)
+        h = h.reshape(n_batch * self._n_particles, 2)
+        edge_attr = torch.sum((x[edges[0]] - x[edges[1]]) ** 2, dim=1, keepdim=True)
+        _, x_final = self.egnn(h, x, edges, edge_attr=edge_attr)
+        vel = x_final - x
+
+        vel = vel.view(n_batch, self._n_particles, self._n_dimensions)
+        vel = remove_mean(vel)
+        # Changed by Leon
+        self.counter += 1
+        return vel.view(n_batch, self._n_particles * self._n_dimensions)
+
+    def _create_edges(self):
+        rows, cols = [], []
+        for i in range(self._n_particles):
+            for j in range(i + 1, self._n_particles):
+                rows.append(i)
+                cols.append(j)
+                rows.append(j)
+                cols.append(i)
+        return [torch.LongTensor(rows), torch.LongTensor(cols)]
+
+    def _cast_edges2batch(self, edges, n_batch, n_nodes, device):
+        if n_batch not in self._edges_dict:
+            self._edges_dict = {}
+            rows, cols = edges
+            rows_total, cols_total = [], []
+            for i in range(n_batch):
+                rows_total.append(rows + i * n_nodes)
+                cols_total.append(cols + i * n_nodes)
+            rows_total = torch.cat(rows_total).to(device)
+            cols_total = torch.cat(cols_total).to(device)
+
+            self._edges_dict[n_batch] = [rows_total, cols_total]
+        return self._edges_dict[n_batch]
