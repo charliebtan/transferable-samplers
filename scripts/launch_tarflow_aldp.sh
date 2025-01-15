b1e8b171369444d38602d13747ca7b573e891aa4
Joey Bose <joey.bosr@gmail.com>
2025-01-14 19:21:07 -0500

    new launcher + sweep

---
+++ scripts/launch_tarflow_aldp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python src/train.py -m launcher=mila_48gb \
+model=normalizing_flow logger=wandb \
+trainer=gpu trainer.max_epochs=1000 \
+model.optimizer._target_=torch.optim.AdamW \
+model.optimizer.weight_decay=0.0001 \
+tags=[tarflow,mle,aldp] \
+model.net.num_blocks=4,8,12,16 \
+model.net.channels=64,128,256,512
