2cb6083220d5ce77f2feb3b4470cc18fecb00382
char-tan <76977995+char-tan@users.noreply.github.com>
2025-01-15 23:49:48 +0000

    fix sweep

--- scripts/launch_tarflow_aldp_epochs.sh
+++ scripts/launch_tarflow_aldp_epochs.sh
@@ -2,12 +2,12 @@
 python src/train.py -m launcher=ox_h100 \
 model=normalizing_flow logger=wandb \
 data=aldp \
-trainer=gpu trainer.max_epochs=1000 \
+trainer=gpu \
 model.optimizer._target_=torch.optim.AdamW \
 model.optimizer.weight_decay=0.0001 \
 tags=[tarflow,mle,aldp,epochs_grid] \
 model.net.num_blocks=4 \
 model.net.channels=256 \
 trainer.check_val_every_n_epoch=20 \
-data.com_augmentation=0 \
++data.com_augmentation=0 \
 trainer.max_epochs=250,500,1000,2000
