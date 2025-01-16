1e8576d4f20d24991ff5c4132e9d96ef076087b3
Joey Bose <joey.bosr@gmail.com>
2025-01-16 14:15:24 -0500

    update sweep, launcher

--- scripts/launch_tarflow_reflow_augs.sh
+++ scripts/launch_tarflow_reflow_augs.sh
@@ -5,7 +5,7 @@ data=aldp \
 trainer=gpu trainer.max_epochs=1000 \
 model.optimizer._target_=torch.optim.AdamW \
 model.optimizer.weight_decay=0.0001 \
-tags=[tarflow,mle,aldp,priors_grid_reflow] \
+tags=[tarflow,mle,aldp,reflow_augs_grid] \
 model.net.num_blocks=4 \
 model.net.channels=256 \
 trainer.check_val_every_n_epoch=50 \
@@ -13,6 +13,5 @@ trainer.check_val_every_n_epoch=50 \
 +data.com_augmentation=1,0 \
 +model.aligned_loss_fn=1,0 \
 model.base_flow_ckpt_path=/home/mila/b/bosejoey/scratch/fast-tbg/logs/train/runs/2025-01-16_08-21-37/checkpoints/last.ckpt \
-data.batch_size=1024 \ 
-model.num_reflow_samples=200_000 \
+data.batch_size=1024 \
 data.num_workers=0
