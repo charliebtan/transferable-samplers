3a3f6ca856ea1f2f4fafa1ae11214ad2da91c20f
Alex Tong <alexandertongdev@gmail.com>
2025-01-17 07:44:38 -0500

    Set eval_non_ema to false by default

--- scripts/alex/cfm.sh
+++ scripts/alex/cfm.sh
@@ -5,6 +5,7 @@ data=aldp,al3,al4 \
 trainer=gpu trainer.max_epochs=1000 \
 tags=[tarflow,mle,al,cnf,v5] \
 trainer.check_val_every_n_epoch=50 \
-data.batch_size=1024 \
+data.batch_size=512 \
+model.sampling_config.batch_size=200 \
 model.net.hidden_nf=256 \
 data.num_workers=2
