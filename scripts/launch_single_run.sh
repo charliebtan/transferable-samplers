1cb4cec0bcff85c173949a70ba4f61a56394f1eb
Joey Bose <joey.bosr@gmail.com>
2025-01-19 10:39:02 -0500

    scripts

--- scripts/launch_single_run.sh
+++ scripts/launch_single_run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 python src/train.py -m launcher=mila_48gb \
 model=normalizing_flow logger=wandb \
-data=al3,al4 \
+data=aldp \
 trainer=gpu trainer.max_epochs=1000 \
 model.optimizer._target_=torch.optim.AdamW \
 model.optimizer.weight_decay=0.0001 \
-tags=[tarflow,mle,al34_model_size_grid] \
-model.net.num_blocks=4,6,8 \
-model.net.layers_per_block=2,4,6 \
-model.net.channels=256,512,1024 \
+tags=[tarflow,mle,best_practice] \
+model.net.num_blocks=4 \
+model.net.layers_per_block=4 \
+model.net.channels=256 \
 trainer.check_val_every_n_epoch=50 \
 +model.force_gaussian_loss=1 \
 model.mean_free_prior=1 \
