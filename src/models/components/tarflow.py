fix normalizing flow logdets

src/models/components/tarflow.py
@@ -199,7 +199,7 @@ class MetaBlock(torch.nn.Module):
             xa = torch.zeros_like(x)
 
         scale = (-xa.float()).exp().type(xa.dtype)
-        return self.permutation((x_in - xb) * scale, inverse=True), -xa.sum(dim=[1, 2])
+        return self.permutation((x_in - xb) * scale, inverse=True), -xa.mean(dim=[1, 2])
 
     def reverse_step(
         self,
@@ -386,7 +386,6 @@ class TarFlow(torch.nn.Module):
             x = block.reverse(x, y, guidance, guide_what, attn_temp, annealed_guidance)
             seq.append(self.unpatchify(x))
         x = self.unpatchify(x)
-        x = x.squeeze()
         if not return_sequence:
             return x
         else:
@@ -412,3 +411,20 @@ if __name__ == "__main__":
 
     assert torch.allclose(x, x_recon), "Invertibility test failed"
     print("Invertibility test passed")
+
+    for i in range(16):
+
+        x_i = x[i:i+1]
+
+        with torch.no_grad():
+            x_pred = model.reverse(x_i)
+            x_recon, fwd_logdets = model(x_pred)
+            fwd_logdets = fwd_logdets * img_size # rescale from mean to sum
+
+        rev_jac_true = torch.autograd.functional.jacobian(model.reverse, x_i, vectorize=True)
+
+        rev_logdets_true = torch.logdet(rev_jac_true.squeeze())
+
+        assert torch.allclose(- fwd_logdets, rev_logdets_true) 
+    print("logdet test passed")
+

src/models/normalizing_flow_module.py
@@ -47,8 +47,9 @@ class NormalizingFlowLitModule(BoltzmannGeneratorLitModule):
         return loss
 
     def proposal_energy(self, x: torch.Tensor) -> torch.Tensor:
-        x_pred, dlogp = self.net.forward(x)
-        return -(-self.prior.energy(x).view(-1) + dlogp.view(-1))
+        x_pred, fwd_logdets = self.net(x)
+        fwd_logdets = fwd_logdets * self.datamodule.dim # rescale from mean to sum
+        return -(-self.prior.energy(x_pred).view(-1) + fwd_logdets.view(-1))
 
     def energy_kl(self, x: torch.Tensor, model_log_p: torch.Tensor) -> torch.Tensor:
         sample_target_energy = self.datamodule.energy(x)
@@ -74,27 +75,14 @@ class NormalizingFlowLitModule(BoltzmannGeneratorLitModule):
 
         self.prior = NormalDistribution(self.datamodule.dim)
 
-        # dist = torch.distributions.multivariate_normal.MultivariateNormal(torch.zeros(self.datamodule.dim), torch.eye(self.datamodule.dim))
-
         prior_samples = self.prior.sample(batch_size).to(self.device)
         prior_log_p = -self.prior.energy(prior_samples)
-        # prior_log_p = - 0.5 * prior_samples.pow(2).sum(dim=-1, keepdim=True)
 
-        # prior_p = torch.exp(prior_log_p)
-        # print(prior_p)
-        # print(torch.max(prior_p))
-
-        # print()
-
-        # print(torch.exp(dist.log_prob(prior_samples.cpu())))
-    
-        # breakpoint()
-
-        # This is a bit slow... but probably fine 2x calls
         with torch.no_grad():
 
             x_pred = self.net.reverse(prior_samples)
-            x_recon, logdets = self.net(x_pred)
+            x_recon, fwd_logdets = self.net(x_pred)
+            fwd_logdets = fwd_logdets * self.datamodule.dim # rescale from mean to sum
 
             self.log("invert/mse", torch.mean((prior_samples - x_recon) ** 2))
             self.log('invert/max_abs', torch.max(abs(prior_samples - x_recon)))
@@ -115,13 +103,7 @@ class NormalizingFlowLitModule(BoltzmannGeneratorLitModule):
                 (torch.sum(abs(prior_samples - x_recon) > cutoff, dim=1) > 0).sum().float()
             )
         
-        log_p = prior_log_p.flatten() + logdets.flatten()
-
-        # print(prior_log_p.view(-1, 1))
-        # print(logdets.view(-1, 1))
-        # print(log_p.view(-1, 1))
-
-        # print(self.datamodule.energy(x_pred).view(-1, 1))
+        log_p = prior_log_p.flatten() + fwd_logdets.flatten()
 
         return x_pred, log_p, torch.empty(0)
 