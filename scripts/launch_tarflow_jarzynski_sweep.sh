2bf4f5f346aebe810df96c57861c5b36b47bd437
Joey Bose <joey.bosr@gmail.com>
2025-01-24 16:21:28 -0500

    update sweep again

--- scripts/launch_tarflow_jarzynski_sweep.sh
+++ scripts/launch_tarflow_jarzynski_sweep.sh
@@ -5,7 +5,7 @@ trainer=gpu \
 tags=[jarz,jarz_sweep] \
 model.jarzynski_sampler.num_timesteps=100 \
 model.sampling_config.energy_cutoff=20.0,0.0 \
-model.jarzynski_sampler.langevin_eps=5e-7,1e-6,2e-6,5e-6,1e-5,2e-5,5e-5 \
+model.jarzynski_sampler.langevin_eps=2e-7,5e-7,1e-6,2e-6,5e-6,1e-5,2e-5,5e-5 \
 model.jarzynski_sampler.ess_threshold=0.95,0.9,0.8,0.6 \ 
 ckpt_path="/home/mila/b/bosejoey/scratch/fast-tbg/logs/train/multiruns/2025-01-23_22-29-10/12/checkpoints/epoch_699_cropped.ckpt","/home/mila/b/bosejoey/scratch/fast-tbg/logs/train/multiruns/2025-01-23_22-29-10/4/checkpoints/epoch_899_cropped.ckpt"
 
