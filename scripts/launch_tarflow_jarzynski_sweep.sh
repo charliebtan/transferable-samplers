8430e73652912d9f58c7e8998ef13ebb45388a14
Joey Bose <joey.bosr@gmail.com>
2025-01-24 17:51:35 -0500

    update sweep

--- scripts/launch_tarflow_jarzynski_sweep.sh
+++ scripts/launch_tarflow_jarzynski_sweep.sh
@@ -7,7 +7,7 @@ model.jarzynski_sampler.num_timesteps=100 \
 model.sampling_config.energy_cutoff=20.0,0.0 \
 model.jarzynski_sampler.langevin_eps=5e-7,1e-6,2e-6,5e-6,1e-5,2e-5 \
 model.jarzynski_sampler.ess_threshold=0.9,0.8,0.7 \
-model.sampling_confing.batch_size=20000 \
+model.sampling_config.batch_size=20000 \
 model.jarzynski_sampler.batch_size=1024 \
 ckpt_path="/home/mila/b/bosejoey/scratch/fast-tbg/logs/train/multiruns/2025-01-23_22-29-10/12/checkpoints/epoch_699_cropped.ckpt","/home/mila/b/bosejoey/scratch/fast-tbg/logs/train/multiruns/2025-01-23_22-29-10/4/checkpoints/epoch_899_cropped.ckpt"
 
