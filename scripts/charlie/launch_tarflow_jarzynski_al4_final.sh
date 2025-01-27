b85cdb37f770859ae48a37b715de65962fa0909c
Joey Bose <joey.bosr@gmail.com>
2025-01-27 18:30:28 -0500

    changes to sweeps

--- scripts/charlie/launch_tarflow_jarzynski_al4_final.sh
+++ scripts/charlie/launch_tarflow_jarzynski_al4_final.sh
@@ -2,9 +2,10 @@
 python src/train.py -m launcher=mila_48gb_short \
 experiment=jarz_tarflow_al4 \
 trainer=gpu \
-tags=[jarz,jarz_final_v4] \
+tags=[jarz,jarz_final_v6] \
 model.jarzynski_sampler.num_timesteps=100 \
 model.sampling_config.energy_cutoff=50 \
 model.jarzynski_sampler.langevin_eps=1e-7 \
-model.jarzynski_sampler.ess_threshold=0.7,0.5 \
+model.jarzynski_sampler.ess_threshold=0.5 \
+seed=0,1,2 \
 ckpt_path="/home/mila/b/bosejoey/scratch/fast-tbg/logs/train/multiruns/2025-01-21_19-56-57/0/checkpoints/epoch_749_cropped.ckpt","/home/mila/b/bosejoey/scratch/fast-tbg/logs/train/multiruns/2025-01-21_19-56-57/1/checkpoints/epoch_999_cropped.ckpt","/home/mila/b/bosejoey/scratch/fast-tbg/logs/train/multiruns/2025-01-21_19-56-57/2/checkpoints/epoch_999_cropped.ckpt"
