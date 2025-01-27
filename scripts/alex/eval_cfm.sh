d31c7dca3e5c85207cd09019f3a6277598382f71
Alex Tong <alexandertongdev@gmail.com>
2025-01-26 22:51:23 -0500

    modify eval

--- scripts/alex/eval_cfm.sh
+++ scripts/alex/eval_cfm.sh
@@ -12,21 +12,21 @@ if [ False = True ]; then
     train=False \
     ckpt_path='${oc.env:AL2_EQ1}/last.ckpt,${oc.env:AL2_EQ2}/last.ckpt,${oc.env:AL2_EQ3}/last.ckpt'
 fi
-if [ True = True ]; then
+if [ False = True ]; then
     python src/train.py -m \
     experiment=aldp logger=wandb \
     data=al3 \
     trainer=gpu trainer.max_epochs=1000 \
     tags=[al,cnf,eval,v9] \
     data.batch_size=512 \
-    model.sampling_config.batch_size=64 \
+    model.sampling_config.batch_size=32 \
     model.sampling_config.num_test_proposal_samples=10000 \
     model.net.hidden_nf=256 \
     data.num_workers=2 \
     train=False \
     ckpt_path='${oc.env:AL3_EQ1}/last.ckpt,${oc.env:AL3_EQ2}/last.ckpt,${oc.env:AL3_EQ3}/last.ckpt'
 fi
-if [ False = True ]; then
+if [ True = True ]; then
     python src/train.py -m \
     experiment=aldp logger=wandb \
     data=al4 \
