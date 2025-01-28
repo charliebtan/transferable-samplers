7854f5a1bc35e64b93eda8a66e2361c5015a5de3
Joey Bose <joey.bosr@gmail.com>
2025-01-27 19:26:25 -0500

    interactive scripts

---
+++ scripts/charlie/launch_jarz_al4_i.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+python src/train.py -m \
+experiment=jarz_tarflow_al4 \
+trainer=gpu \
+tags=[jarz,jarz_final_i] \
+model.jarzynski_sampler.num_timesteps=100 \
+model.sampling_config.energy_cutoff=50 \
+model.jarzynski_sampler.langevin_eps=1e-7 \
+model.jarzynski_sampler.ess_threshold=0.5 \
+model.jarzynski_sampler.batch_size=3076 \
+seed=0,1,2 \
+ckpt_path="/home/mila/b/bosejoey/scratch/fast-tbg/logs/train/multiruns/2025-01-21_19-56-57/0/checkpoints/epoch_749_cropped.ckpt"
