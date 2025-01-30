a527387af99e52a5771a7b23feafa336ba081ab6
Joey Bose <joey.bosr@gmail.com>
2025-01-29 22:38:58 -0500

    scripts

---
+++ scripts/charlie/30th_jan/launch_jarz_al5_i.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=$1
+python src/train.py -m \
+experiment=jarz_tarflow_al5 \
+trainer=gpu \
+tags=[jarz,jarz_big] \
+model.jarzynski_sampler.num_timesteps=100 \
+model.sampling_config.energy_cutoff=-40.0 \
+model.jarzynski_sampler.langevin_eps=1e-7 \
+model.jarzynski_sampler.ess_threshold=0.5 \
+model.use_com_energy=1,0 \
+seed=0,1,2
