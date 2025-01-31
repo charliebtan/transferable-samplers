483fe483942a67f494fee95b7b59cb3c9b206a94
Joey Bose <joey.bosr@gmail.com>
2025-01-30 23:57:39 -0500

    update scripts

--- scripts/charlie/30th_jan/launch_jarz_chignolin.sh
+++ scripts/charlie/30th_jan/launch_jarz_chignolin.sh
@@ -7,5 +7,5 @@ tags=[jarz,jarz_big] \
 model.jarzynski_sampler.num_timesteps=100 \
 model.sampling_config.energy_cutoff=-100.0 \
 model.jarzynski_sampler.langevin_eps=1e-9 \
-model.jarzynski_sampler.ess_threshold=0.3 \
+model.jarzynski_sampler.ess_threshold=0.5 \
 model.use_com_energy=1
