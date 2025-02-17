f658d10f42c1dde901be1af9aac2d443d1c90a90
Joey Bose <joey.bosr@gmail.com>
2025-02-17 13:31:04 -0500

    scripts

--- scripts/charlie/16th_feb/launch_jarz_al6.sh
+++ scripts/charlie/16th_feb/launch_jarz_al6.sh
@@ -8,6 +8,6 @@ model.jarzynski_sampler.num_timesteps=100 \
 model.sampling_config.energy_cutoff=-20.0 \
 model.jarzynski_sampler.langevin_eps=1e-7 \
 model.jarzynski_sampler.ess_threshold=0.5 \
-model.jarzynski_sampler.batch_size=1024 \
+model.jarzynski_sampler.batch_size=512 \
 model.use_com_energy=1 \
 ckpt_path="/network/scratch/b/bosejoey/fast-tbg/logs/train/runs/tarflow_al6_v2/checkpoints/epoch_999_time.ckpt"
