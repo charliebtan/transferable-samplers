8433d1c9148fa58abf760916cf8ea30fc47cd26d
Alex Tong <alexandertongdev@gmail.com>
2025-03-10 06:15:30 -0400

    Add updated scripts

--- scripts/alex/al6_cnf.sh
+++ scripts/alex/al6_cnf.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
-#SBATCH -J train_nf                 # Job name
+#SBATCH -J train_al6                 # Job name
 #SBATCH -o watch_folder/%x_%j.out     # output file (%j expands to jobID)
 #SBATCH -N 1                          # Total number of nodes requested
 #SBATCH --get-user-env                # retrieve the users login environment
-#SBATCH --mem=128G                     # server memory requested (per node)
-#SBATCH -t 3:00:00                  # Time limit (hh:mm:ss)
-#SBATCH --partition=short-unkillable               # Request partition
-#SBATCH --ntasks-per-node=4
-#SBATCH --gres=gpu:l40s:4                  # Type/number of GPUs needed
-#SBATCH -c 12
+#SBATCH --mem=48G                     # server memory requested (per node)
+#SBATCH -t 5-00:00:00                  # Time limit (hh:mm:ss)
+#SBATCH --partition=main
+#SBATCH --ntasks-per-node=2
+#SBATCH --gres=gpu:a100l:2                  # Type/number of GPUs needed
+#SBATCH -c 4
 #SBATCH --open-mode=append            # Do not overwrite logs
 #SBATCH --requeue                     # Requeue upon pre-emption
 #SBATCH --signal=SIGUSR1@90
 
-RUN_NAME="al6_eqcnf_v3"
-srun python -u src/train.py experiment=aldp logger=wandb seed=42 data=al6 trainer=ddp \
+RUN_NAME="al6_eqcnf_v3_seed_44"
+#srun python -u src/train.py \
+python -u src/train.py \
+  experiment=aldp logger=wandb seed=44 data=al6 trainer=ddp \
 model/net=egnn_dynamics_ad2_cat tags=[al,cnf,v12,egnn] \
   trainer.check_val_every_n_epoch=200 data.batch_size=512 \
   trainer.max_epochs=1000 \
   +model.net.pdb_filename='${data.pdb_filename}' \
-  model.sampling_config.batch_size=20 +trainer.num_sanity_val_steps=1 \
-  model.sampling_config.num_proposal_samples=160 model.net.hidden_nf=256 \
+  model.sampling_config.batch_size=40 +trainer.num_sanity_val_steps=1 \
+  model.sampling_config.num_proposal_samples=20 model.net.hidden_nf=256 \
+  model.sampling_config.num_test_proposal_samples=10000 \
   trainer.strategy=ddp_find_unused_parameters_true data.num_workers=6 \
 hydra.run.dir='${paths.log_dir}/${task_name}/runs/'${RUN_NAME} \
 ckpt_path='${paths.log_dir}/${task_name}/runs/'${RUN_NAME}/checkpoints/last.ckpt \
-logger.wandb.id=${RUN_NAME} \
-seed=0
+logger.wandb.id=${RUN_NAME}
--- scripts/alex/old_cfm.sh
+++ scripts/alex/old_cfm.sh
@@ -1,2 +1,7 @@
-python src/train.py -m experiment=ecnf_al3 launcher=mila_48gb_long data=aldp,al3,al4 \
-  trainer=gpu trainer.check_val_every_n_epoch=100 seed=42,43,44 tags="[old_cfm,v2]"
+python src/train.py experiment=ecnf_al3 data=aldp \
+  trainer=gpu trainer.check_val_every_n_epoch=100  \
+  train=False \
+  ckpt_path=/network/scratch/a/alexander.tong/fast-tbg/logs/train/multiruns/2025-02-15_11-32-09/0/checkpoints/epoch_999.ckpt \
+  model.sampling_config.num_test_proposal_samples=100 
+#python src/train.py -m experiment=ecnf_al3 launcher=mila_48gb_long data=al3 \
+#  trainer=gpu trainer.check_val_every_n_epoch=100 seed=42,43,44 tags="[old_cfm,v3]"
