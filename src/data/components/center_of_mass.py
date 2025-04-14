fixed the rotation bug, training code runs

src/data/components/center_of_mass.py
@@ -8,20 +8,36 @@ class CenterOfMassTransform(torch.nn.Module):
         self.std = std
 
     def forward(self, data):
-        assert len(data.shape) == 1, "only process single molecules"
-        data = data.reshape(-1, self.num_dimensions)
+        x = data["x"]
+        mask = data["mask"]
+
+        assert len(x.shape) == 1, "only process single molecules"
+
+        num_particles = mask.sum()
+
+        x = x.reshape(-1, self.num_dimensions)
+
+        x, padding = x[:num_particles], x[num_particles:]  # slice out the data and padding
+
+        assert torch.sum(padding) == 0, "padding should be zero"
+
         # Calculate the current center of mass
-        center_of_mass = data.mean(dim=0)
+        center_of_mass = x.mean(dim=0)
 
         # Generate noise and adjust the center of mass
         noise = torch.randn_like(center_of_mass) * self.std
         new_center_of_mass = center_of_mass + noise
 
         # Shift all particles so that the center of mass is moved
-        data = data + (new_center_of_mass - center_of_mass)
+        x = x + (new_center_of_mass - center_of_mass)
+
+        x = torch.cat([x, padding])  # re-add the padding back
 
         # Reshape back to original shape
-        data = data.reshape(-1)
+        x = x.reshape(-1)
+
+        data.update({"x": x})
+
         return data
 
 

src/data/components/peptide_dataset.py
@@ -9,7 +9,7 @@ class PeptideDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         sample = self.data[idx]
         if self.transform is not None:
-            sample = {**sample, "x": self.transform(sample["x"])}
+            sample = self.transform(sample)
         return sample
 
     def __len__(self):