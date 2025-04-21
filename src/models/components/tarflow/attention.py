a4ad075b4bd1f80060b3aafcacd4f984adfbc4cb
majhas <majdi.mhas@gmail.com>
2025-04-21 14:28:40 -0400

    add spda compatible implementation

--- src/models/components/tarflow/attention.py
+++ src/models/components/tarflow/attention.py
@@ -17,6 +17,8 @@ max_neg_value = lambda x: torch.finfo(x.dtype).min
 
 
 class Attention(torch.nn.Module):
+    USE_SPDA = True
+
     def __init__(
         self,
         in_channels: int,
@@ -47,7 +49,7 @@ class Attention(torch.nn.Module):
         self.k_cache: dict[str, list[torch.Tensor]] = {"cond": [], "uncond": []}
         self.v_cache: dict[str, list[torch.Tensor]] = {"cond": [], "uncond": []}
 
-    def forward(
+    def forward_spda(
         self,
         x,
         pair: torch.Tensor | None = None,
@@ -55,8 +57,57 @@ class Attention(torch.nn.Module):
         temp: float = 1.0,
         which_cache: str = "cond",
     ):
-        if self.use_pair_bias and exists(pair) is None:
+        if (self.use_pair_bias and not exists(pair)) or (not self.use_pair_bias and exists(pair)):
             raise ValueError("pair must be provided if use_pair_bias is True")
+
+        B, T, C = x.size()
+        x = self.norm(x.float()).type(x.dtype)
+        pair = self.pair_norm(pair) if exists(pair) else None
+        q, k, v = self.qkv(x).chunk(3, dim=-1)
+        q = self.q_layer_norm(q)
+        k = self.k_layer_norm(k)
+
+        bias = rearrange(self.bias_proj(pair), "b ... h -> b h ...") if (exists(pair) and self.use_pair_bias) else 0.0
+        q, k, v = map(lambda t: rearrange(t, "b ... (h d) -> b h ... d", h=self.num_heads), (q, k, v))
+
+        if self.sample:
+            self.k_cache[which_cache].append(k)
+            self.v_cache[which_cache].append(v)
+            k = torch.cat(self.k_cache[which_cache], dim=2)  # note that sequence dimension is now 2
+            v = torch.cat(self.v_cache[which_cache], dim=2)
+
+        scale = self.sqrt_scale**2 / temp
+        if mask is not None:
+            mask = mask.bool()
+
+        if self.use_pair_bias:
+            if mask is not None:
+                if mask.ndim < 4:
+                    mask = mask.reshape(*([1] * (4 - mask.ndim)), *mask.shape)
+
+                attn_mask = torch.zeros_like(mask, dtype=q.dtype)
+                attn_mask.masked_fill_(mask.logical_not(), float("-inf"))
+                mask = attn_mask + bias
+            else:
+                mask = bias
+
+        x = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=mask, scale=scale)
+        x = x.transpose(1, 2).reshape(B, T, C)
+        x = self.proj(x)
+        return x
+
+    def forward_base(
+        self,
+        x,
+        pair: torch.Tensor | None = None,
+        mask: torch.Tensor | None = None,
+        temp: float = 1.0,
+        which_cache: str = "cond",
+    ):
+        if (self.use_pair_bias and not exists(pair)) or (not self.use_pair_bias and exists(pair)):
+            print(self.use_pair_bias, exists(pair))
+            raise ValueError("pair must be provided if use_pair_bias is True")
+
         x = self.norm(x.float()).type(x.dtype)
         pair = self.pair_norm(pair) if exists(pair) else None
         q, k, v = self.qkv(x).chunk(3, dim=-1)
@@ -97,6 +148,19 @@ class Attention(torch.nn.Module):
         attn = torch.softmax(sim, dim=-1).type(sim.dtype)
         attn = torch.dropout(attn, self.dropout, train=self.sample)
         return einsum("b h i j, b h j d -> b h i d", attn, v)
+
+    def forward(
+        self,
+        x,
+        pair: torch.Tensor | None = None,
+        mask: torch.Tensor | None = None,
+        temp: float = 1.0,
+        which_cache: str = "cond",
+    ):
+        if self.USE_SPDA:
+            return self.forward_spda(x, pair=pair, mask=mask, temp=temp, which_cache=which_cache)
+
+        return self.forward_base(x, pair=pair, mask=mask, temp=temp, which_cache=which_cache)
 
 
 class MLP(torch.nn.Module):
@@ -163,30 +227,24 @@ if __name__ == "__main__":
     torch.set_printoptions(sci_mode=True, precision=2)
     torch.manual_seed(1)
 
-    attn = Attention(128, 64, use_qkln=False)
+    attn = Attention(128, 64, use_qkln=True, use_pair_bias=True)
 
     x = torch.randn((128, 10, 128))
     mask = torch.tril(torch.ones((x.shape[1], x.shape[1]), dtype=torch.bool))
-    y = attn(x, mask=mask)
+    pair = torch.randn((128, 10, 10, 128))
+    y = attn(x, pair=pair, mask=mask)
 
     attn.USE_SPDA = False
-    z = attn(x, mask=mask)
+    z = attn(x, pair=pair, mask=mask)
 
     error = (abs(y - z)).mean()
-    print(f"Error between SPDA and base: {error}")
+    print(f"Error between SPDA and proteina: {error}")
     assert torch.allclose(y, z, atol=1e-6), f"Error: {error}"
 
-    w = attn(x, mask=mask)
+    w = attn(x, pair=pair, mask=mask)
     assert not torch.isnan(w).any()
     assert not torch.isinf(w).any()
 
-    error_base_pro = abs(z - w).mean()
-    error_spda_pro = (abs(y - w)).mean()
-    print(f"Error between proteina and base: {error_base_pro}")
-    print(f"Error between proteina and SPDA: {error_spda_pro}")
-    # should pass if x is not multiplied with g
-    assert torch.allclose(y, w, atol=1e-7)
-
-    pair = torch.randn((128, 10, 10, 128))
-    w = attn(x, pair=pair, mask=mask)
-    assert not torch.allclose(w, z)
+    # pair = torch.randn((128, 10, 10, 128))
+    # w = attn(x, pair=pair, mask=mask)
+    # assert not torch.allclose(w, z)
