6e2d4aec350ce9dcd19e46eab3ef7e136258954b
char-tan <76977995+charliebtan@users.noreply.github.com>
2025-05-01 16:33:35 -0400

    fix

--- scripts/charlie/training/sbatch/tamia/tarflow_up_to_4aa_tamia.sh
+++ scripts/charlie/training/sbatch/tamia/tarflow_up_to_4aa_tamia.sh
@@ -27,6 +27,7 @@ experiment=training/tarflow_up_to_4aa logger=wandb \
 trainer=ddp \
 data.data_dir='/project/aip-necludov/shared/self-consume-bg/data/new' \
 data.batch_size=512 \
+data.train_lmdb_prefix='train_medium_up_to_4aa' \
 tags=[up_to_4aa,ddp] \
 hydra.run.dir='${paths.log_dir}/${task_name}/runs/'${RUN_NAME} \
 ckpt_path='${paths.log_dir}/${task_name}/runs/'${RUN_NAME}/checkpoints/last.ckpt \
