6d343c5ad4de175bb2e54565fb6390ff7755a318
char-tan <76977995+charliebtan@users.noreply.github.com>
2025-05-05 14:48:34 -0400

    update scripts

--- scripts/charlie/training/sbatch/tamia/ecnf_2aa_tamia.sh
+++ scripts/charlie/training/sbatch/tamia/ecnf_2aa_tamia.sh
@@ -19,16 +19,21 @@ module load openmm/8.2.0
 module load httpproxy/1.0
 source $HOME/envs/$env/bin/activate
 
+wandb online
+
 RUN_NAME="encf_2aa_v1"
 
 # ddp_find_unused_parameters_true needed because some of modules in final layer don't affect loss
 
 srun python -u src/train.py \
 experiment=training/ecnf_2aa logger=wandb \
-trainer=ddp_find_unused_parameters_true \
+trainer=ddp \
+trainer.strategy=ddp_find_unused_parameters_true \
 data.data_dir='/project/aip-necludov/shared/self-consume-bg/data/new' \
 data.batch_size=512 \
 data.train_lmdb_prefix='train_medium_up_to_4aa' \
+data.val_lmdb_prefix='new_val' \
+data.test_lmdb_prefix='new_test' \
 tags=[2aa,ddp,cfm] \
 trainer.check_val_every_n_epoch=10000 \
 trainer.num_sanity_val_steps=0 \
