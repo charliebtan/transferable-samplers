67fd3b73a39530859a9596c22f2781151a9c3f2e
majhas <majdi.mhas@gmail.com>
2025-05-07 01:54:06 -0400

    add scripts for smc sampling

---
+++ scripts/charlie/evaluation/submitit/tarflow_up_to_4aa_hmc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+python src/train.py -m launcher=mila_l40_main \
+experiment=evaluation/tarflow_up_to_4aa_hmc \
+trainer=gpu \
+tags=[sampling,eval_tarflow_up_to_4aa,smc,hmc,dev] \
+data.train_lmdb_prefix="train_medium_v2" \
+data.val_lmdb_prefix=val \
+data.test_lmdb_prefix=test \
+model.smc_sampler.num_timesteps=1000 \
+model.smc_sampler.langevin_eps=1e-8 \
+model.smc_sampler.ess_threshold=0.9 \
+model.smc_sampler.batch_size=256 \
++model.smc_sampler.systematic_resampling=True \
+model.smc_sampler.input_energy_cutoff=100 \
+seed=0 \
+ckpt_path="/network/scratch/m/majdi.hassan/self-consume-bg/self-consume-bg/logs/train/runs/tarflow_up_to_4aa_v2/checkpoints/last.ckpt" \
++model.eval_seq_name="QDED"
