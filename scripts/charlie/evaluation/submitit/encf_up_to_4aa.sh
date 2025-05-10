4c44d38a2efbbfce7a7c390d738f1c26c01f1b99
char-tan <76977995+charliebtan@users.noreply.github.com>
2025-05-10 02:20:39 -0400

    fix script

--- scripts/charlie/evaluation/submitit/encf_up_to_4aa.sh
+++ scripts/charlie/evaluation/submitit/encf_up_to_4aa.sh
@@ -5,5 +5,4 @@ tags=[up_to_4aa,ecnf_eval_v1] \
 logger=wandb \
 model.eval_seq_name="AC","AT","ET","GN","GP","HT","IM","KG","KQ","KS","LW","NF","NY","RL","RV","TD","SAEL","RYDT","CSFQ","FALS","CSGS","LPEM","LYVI","AYTG","VCVS","AAEW","FKVP","NQFM","DTDL","CTSA","ANYT","VTST","AWKC","RGSP","AVEK","FIYG","VLSM","QADY","DQAL","TFFL","FIGE","KKQF","SLTC","ITQD","DFKS","QDED" \
 seed="range(0, 10)" \
-hydra.run.dir="/network/scratch/t/tanc/self-consume-bg/logs/train/runs/encf_up_to_4aa_v1_eval" \
-model.sampling_config.num_proposal_samples=1_000
+model.sampling_config.num_test_proposal_samples=1_000
