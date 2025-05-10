c6c08dc74b6a9455a792a8231a56067228db67b5
majhas <majdi.mhas@gmail.com>
2025-05-09 23:38:40 -0400

    moved settings to config

--- scripts/charlie/evaluation/submitit/tarflow_up_to_4aa_mala.sh
+++ scripts/charlie/evaluation/submitit/tarflow_up_to_4aa_mala.sh
@@ -2,19 +2,13 @@
 python src/train.py -m launcher=mila_48gb \
 experiment=evaluation/tarflow_up_to_4aa_mala \
 trainer=gpu \
-tags=[sampling,eval_tarflow_up_to_4aa,smc,mala,dev] \
+tags=[eval_tarflow_up_to_4aa,sampling,smc,mala] \
 data.train_lmdb_prefix="train_medium" \
 data.val_lmdb_prefix=val \
 data.test_lmdb_prefix=test \
-model.smc_sampler.num_timesteps=100 \
-model.smc_sampler.langevin_eps=1e-4 \
-model.smc_sampler.ess_threshold=0.5 \
 model.smc_sampler.batch_size=256 \
-model.smc_sampler.log_freq=1 \
-+model.smc_sampler.systematic_resampling=True \
 model.smc_sampler.input_energy_cutoff=100 \
 seed=0 \
 ckpt_path="/network/scratch/m/majdi.hassan/self-consume-bg/self-consume-bg/logs/train/runs/tarflow_up_to_4aa_v2/checkpoints/last.ckpt" \
-+model.eval_seq_name="RSCR"
-
-# ,"TAQE","RGGF","TGRC","CWVY"
++model.eval_seq_name="RSCR","TAQE","RGGF","TGRC","CWVY" \
++trainer.limit_val_batches=1
