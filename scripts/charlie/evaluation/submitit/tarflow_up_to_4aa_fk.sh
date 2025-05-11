058d5396a9f838ad35fdd36ad9938acf272e43aa
majhas <majdi.mhas@gmail.com>
2025-05-11 19:46:07 -0400

    fix

--- scripts/charlie/evaluation/submitit/tarflow_up_to_4aa_fk.sh
+++ scripts/charlie/evaluation/submitit/tarflow_up_to_4aa_fk.sh
@@ -11,6 +11,6 @@ data.val_lmdb_prefix=val \
 data.test_lmdb_prefix=test \
 model.smc_sampler.batch_size=256 \
 model.smc_sampler.input_energy_cutoff=100 \
-model.eval_seq_name=model.eval_seq_name="AC","AT","ET","GN","GP","HT","IM","KG","KQ","KS","LW","NF","NY","RL","RV","TD","SAEL","RYDT","CSFQ","FALS","CSGS","LPEM","LYVI","AYTG","VCVS","AAEW","FKVP","NQFM","DTDL","CTSA","ANYT","VTST","AWKC","RGSP","AVEK","FIYG","VLSM","QADY","DQAL","TFFL","FIGE","KKQF","SLTC","ITQD","DFKS","QDED"  \
+model.eval_seq_name="AC","AT","ET","GN","GP","HT","IM","KG","KQ","KS","LW","NF","NY","RL","RV","TD","SAEL","RYDT","CSFQ","FALS","CSGS","LPEM","LYVI","AYTG","VCVS","AAEW","FKVP","NQFM","DTDL","CTSA","ANYT","VTST","AWKC","RGSP","AVEK","FIYG","VLSM","QADY","DQAL","TFFL","FIGE","KKQF","SLTC","ITQD","DFKS","QDED"  \
 seed=0 \
 ckpt_path="/network/scratch/m/majdi.hassan/self-consume-bg/self-consume-bg/logs/train/runs/tarflow_up_to_4aa_v2/checkpoints/last.ckpt" \
