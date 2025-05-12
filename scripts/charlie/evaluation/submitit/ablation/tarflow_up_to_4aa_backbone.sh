7374932752d2fc6b7b2d9b0a5ae2a25c9d8d51d7
majhas <majdi.mhas@gmail.com>
2025-05-11 23:59:55 -0400

    v1 -> v2

--- scripts/charlie/evaluation/submitit/ablation/tarflow_up_to_4aa_backbone.sh
+++ scripts/charlie/evaluation/submitit/ablation/tarflow_up_to_4aa_backbone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python src/train.py -m launcher=mila_48gb \
 experiment=evaluation/tarflow_up_to_4aa \
-tags=[up_to_4aa,eval,ablation_v1,backbone] \
+tags=[up_to_4aa,eval,ablation_v2,backbone] \
 logger=wandb \
 model.net.perm_type="globloc" \
 model.eval_seq_name="AC","AT","ET","GN","GP","HT","IM","KG","KQ","KS","LW","NF","NY","RL","RV","TD","SAEL","RYDT","CSFQ","FALS","CSGS","LPEM","LYVI","AYTG","VCVS","AAEW","FKVP","NQFM","DTDL","CTSA","ANYT","VTST","AWKC","RGSP","AVEK","FIYG","VLSM","QADY","DQAL","TFFL","FIGE","KKQF","SLTC","ITQD","DFKS","QDED" \
