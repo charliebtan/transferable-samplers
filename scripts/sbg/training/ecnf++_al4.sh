3f9d33e91d76896bbb2ac7160830c3eb94983f8e
char-tan <76977995+charliebtan@users.noreply.github.com>
2025-06-03 00:28:59 -0400

    update scripts

--- scripts/sbg/training/ecnf++_al4.sh
+++ scripts/sbg/training/ecnf++_al4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python src/train.py -m launcher=mila_l40 \
 experiment=training/ecnf++_al4 \
-tags=[sgb,al4,ecnf++_sweep_v2] \
-model.optimizer.weight_decay=1e-2,1e-3,1e-4 \
+tags=[sgb,al4,ecnf++_sweep_v3] \
+model.optimizer.weight_decay=1e-4 \
 logger=wandb \
 seed=0,1,2
--- scripts/sbg/training/ecnf++_al6.sh
+++ scripts/sbg/training/ecnf++_al6.sh
@@ -2,6 +2,6 @@
 python src/train.py -m launcher=mila_l40 \
 experiment=training/ecnf++_al6 \
 tags=[sgb,al6,ecnf++_sweep_v3] \
-model.optimizer.weight_decay=1e-2,1e-3,1e-4 \
+model.optimizer.weight_decay=1e-4 \
 logger=wandb \
 seed=0,1,2
