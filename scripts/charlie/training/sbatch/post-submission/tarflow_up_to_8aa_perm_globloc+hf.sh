81eb2602e56a8b9f26351842eaa71f35fdb1e1b9
char-tan <76977995+charliebtan@users.noreply.github.com>
2025-06-29 15:46:28 -0400

    scripts

--- scripts/charlie/training/sbatch/post-submission/tarflow_up_to_8aa_perm_globloc+hf.sh
+++ scripts/charlie/training/sbatch/post-submission/tarflow_up_to_8aa_perm_globloc+hf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J extrapolate_1000                 # Job name
+#SBATCH -J perm_globloc+hf                 # Job name
 #SBATCH -o watch_folder/%x_%j.out     # output file (%j expands to jobID)
 #SBATCH -N 2                          # Total number of nodes requested
 #SBATCH --mem=256G                     # server memory requested (per node)
@@ -21,7 +21,7 @@ source $HOME/envs/$env/bin/activate
 wandb online
 
 echo $SLURM_NNODES
-RUN_NAME="tarflow_up_to_8aa_extrapolate_1000_v5"
+RUN_NAME="tarflow_up_to_8aa_perm_globloc+hf_v1"
 
 srun python -u src/train.py \
 experiment=training/tarflow_up_to_8aa logger=wandb \
@@ -32,10 +32,13 @@ trainer.limit_train_batches=1000 \
 model.net.use_adapt_ln=True \
 model.net.use_transition=True \
 model.net.use_attn_pair_bias=False \
-model.net.perm_type=new \
+model.net.perm_type="globloc+hf" \
 +model.net.cond_embed.sinusoid_div_value=1000 \
 trainer.num_nodes=$SLURM_NNODES \
-tags=[up_to_8aa,ddp,full,extrapolate] \
+trainer.check_val_every_n_epoch=50 \
+model.sampling_config.num_proposal_samples=10_000 \
+model.sampling_config.clip_reweighting_logits=0.002 \
+tags=[up_to_8aa,ddp,full,perms] \
 hydra.run.dir='${paths.log_dir}/${task_name}/runs/'${RUN_NAME} \
 ckpt_path='${paths.log_dir}/${task_name}/runs/'${RUN_NAME}/checkpoints/last.ckpt \
 logger.wandb.id=${RUN_NAME}
